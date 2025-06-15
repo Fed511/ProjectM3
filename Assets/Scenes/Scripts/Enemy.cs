e766e4a56bd486387bdd67e95bbdb66dc3aee0e4
Fed511 <federicouni350@gmail.com>
2025-06-15 22:27:30 +0200

    enemy

--- Assets/Scenes/Scripts/Enemy.cs
+++ Assets/Scenes/Scripts/Enemy.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 3f;
+    private Transform playerTransform;
+
+    private void Start()
     {
-        
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (playerTransform)
+        {
+            Vector2 direction = (playerTransform.position - transform.position).normalized;
+            transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            var life = collision.gameObject.GetComponent<LifeController>();
+            if (life)
+            {
+                life.TakeDamage(10);
+            }
+            if (gameObject.CompareTag("Enemy"))
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
